dd7eef10242f5c0e043c45e4e2c9915e0f4acc8c
perfoo <perfoo@yahoo.com>
2021-06-17 18:55:40 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+var fizzBuzz = [];
+
+for (var i = 0; i <= 20; i++) {
+    if (i % 3 === 0 && i % 5 !== 0) {
+        fizzBuzz.push("fizz");
+    }
+    else if (i % 5 === 0 && i % 3 !== 0) {
+        fizzBuzz.push("buzz");
+    }
+    else if (i !== 0 && i % 15 === 0) {
+        fizzBuzz.push("fizzbuzz");
+    }
+}
+console.log(fizzBuzz);
